fe71d713933bc0630f29f7cbf7997a7026f84e52
Tom Dyas <tom.dyas@gmail.com>
2022-11-25 19:57:49 -0500

    add example of using race detector

---
+++ race/racy.go
@@ -0,0 +1,17 @@
+// From example in https://go.dev/blog/race-detector
+package main
+
+import "fmt"
+
+func main() {
+    done := make(chan bool)
+    m := make(map[string]string)
+    m["name"] = "world"
+    go func() {
+        m["name"] = "data race"
+        done <- true
+    }()
+    fmt.Println("Hello,", m["name"])
+    <-done
+}
+
